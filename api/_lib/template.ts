61c4d31a2f956b5d924c1f76cda11ac49df17cb8
jaredgorski <jared.gorski@liferay.cloud>
2022-01-24 13:59:16 -0800

    Customize fonts

--- api/_lib/template.ts
+++ api/_lib/template.ts
@@ -7,8 +7,8 @@ const twemoji = require('twemoji');
 const twOptions = { folder: 'svg', ext: '.svg' };
 const emojify = (text: string) => twemoji.parse(text, twOptions);
 
-const rglr = readFileSync(`${__dirname}/../_fonts/Inter-Regular.woff2`).toString('base64');
-const bold = readFileSync(`${__dirname}/../_fonts/Inter-Bold.woff2`).toString('base64');
+const rglr = readFileSync(`${__dirname}/../_fonts/PublicSans-Medium.woff2`).toString('base64');
+const bold = readFileSync(`${__dirname}/../_fonts/PublicSans-Bold.woff2`).toString('base64');
 const mono = readFileSync(`${__dirname}/../_fonts/Vera-Mono.woff2`).toString('base64');
 
 function getCss(theme: string, fontSize: string) {
@@ -21,14 +21,14 @@ function getCss(theme: string, fontSize: string) {
     }
     return `
     @font-face {
-        font-family: 'Inter';
+        font-family: 'Public Sans';
         font-style:  normal;
         font-weight: normal;
         src: url(data:font/woff2;charset=utf-8;base64,${rglr}) format('woff2');
     }
 
     @font-face {
-        font-family: 'Inter';
+        font-family: 'Public Sans';
         font-style:  normal;
         font-weight: bold;
         src: url(data:font/woff2;charset=utf-8;base64,${bold}) format('woff2');
@@ -91,7 +91,7 @@ function getCss(theme: string, fontSize: string) {
     }
     
     .heading {
-        font-family: 'Inter';
+        font-family: sans-serif, 'Public Sans';
         font-size: ${sanitizeHtml(fontSize)};
         font-style: normal;
         color: ${foreground};
